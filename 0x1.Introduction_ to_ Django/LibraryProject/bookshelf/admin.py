3b1d7ccaf0e61ca1c4539134069247da009641d1
jr-maziya <geoffmaziya04@gmail.com>
2024-08-11 22:40:14 +0200

    admin.py change

--- 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/admin.py
+++ 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/admin.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 admin.site.register(Book)
 "register", "admin.ModelAdmin"
 "list_filter", "author", "publication_year"
+"search_fields", "title"
 
 # Register your models here.
