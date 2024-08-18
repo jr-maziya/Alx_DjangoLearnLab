982cf6d7ff6f008a71317e35eaec2adfc7faa975
jr-maziya <geoffmaziya04@gmail.com>
2024-08-18 16:56:43 +0200

    initial commit

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -15,3 +15,5 @@ class Library(models.Model):
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+return self.name
