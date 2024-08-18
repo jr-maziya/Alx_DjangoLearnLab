28b63fea68a30db09ecb1dbb0c70b69f07cd65b2
jr-maziya <geoffmaziya04@gmail.com>
2024-08-18 17:04:23 +0200

    initial commit

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Author(models.Model):
     name = models.CharField(max_length=100)
+    "return self.name"
 
 class Book(models.Model):
     name = models.CharField(max_length=100)
@@ -16,4 +17,3 @@ class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
 
-"return self.name"
