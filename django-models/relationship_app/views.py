b9c768caefa009672075636ea73fd35a35168213
jr-maziya <geoffmaziya04@gmail.com>
2024-08-18 21:20:50 +0200

    customized access

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -14,6 +14,10 @@ from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.edit import CreateView
 from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+from django.shortcuts import render, get_object_or_404, redirect
+from models import Book
+from forms import BookForm
 
 # Create your views here.
 def book_list_view(request):
@@ -81,3 +85,37 @@ def librarian_view(request):
 @user_passes_test(lambda user: check_role(user, 'Member'))
 def member_view(request):
     return render(request, 'relationship_app/member_view.html')
+
+# Add book view
+@permission_required('relationship_app.can_add_book', raise_exception=True)
+def add_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+    return render(request, 'books/add_book.html', {'form': form})
+
+# Edit book view
+@permission_required('relationship_app.can_change_book', raise_exception=True)
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'books/edit_book.html', {'form': form})
+
+# Delete book view
+@permission_required('relationship_app.can_delete_book', raise_exception=True)
+def delete_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('book_list')
+    return render(request, 'books/delete_book.html', {'book': book})
