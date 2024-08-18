06e5dcc8b56ebbcca6e418be4978b22f67117158
jr-maziya <geoffmaziya04@gmail.com>
2024-08-18 21:22:01 +0200

    urls update

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
     path('admin/', admin_view, name='admin_view'),
     path('librarian/', librarian_view, name='librarian_view'),
     path('member/', member_view, name='member_view'),
-    path('books/add/', add_book, name='add_book'),
-    path('books/edit/<int:book_id>/', edit_book, name='edit_book'),
-    path('books/delete/<int:book_id>/', delete_book, name='delete_book'),
+    path('books/add_book/', add_book, name='add_book'),
+    path('books/edit_book/<int:book_id>/', edit_book, name='edit_book'),
+    path('books/delete_book/<int:book_id>/', delete_book, name='delete_book'),
 ]
