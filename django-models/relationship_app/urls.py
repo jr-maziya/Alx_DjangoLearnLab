e866d07a03c1219a51aace7589f1039003a40939
jr-maziya <geoffmaziya04@gmail.com>
2024-08-18 19:11:43 +0200

    login and signup

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from . import views
 from .views import list_books, LibraryDetailView
 
+
 urlpatterns = [
     path('', views.book_list_view, name='home'),
-    path('', views.LibraryDetails_view, name='home')
+    path('', views.LibraryDetails_view, name='home'),
+    path(views.register", "LogoutView.as_view template_name=", "LoginView.as_view template_name=")
 ]
